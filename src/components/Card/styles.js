56852db8964d9d0c4384efea77eac7253a4865fc
Leonardo-FAnddrade <leonardo.andrade99@hotmail.com>
2024-07-03 22:02:04 -0300

    Card

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,5 +1,46 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.button`
+  width: 100%;
+  height: 240px;
 
+  border: none;
+  background: rgba(
+    255,
+    133,
+    155,
+    0.05
+  ); /* Usar RGBA para definir a opacidade do fundo */
+  border-radius: 8px;
+  border-radius: 8px;
+  padding: 32px;
+  color: ${({ theme }) => theme.COLORS.WHITE};
+  margin-bottom: 24px;
+  display: flex;
+  flex-direction: column;
+
+  > h1 {
+    margin-bottom: 8px;
+  }
+
+  .star {
+    display: flex;
+    gap: 4px;
+    margin-bottom: 8px;
+    > svg {
+      margin-bottom: 12px;
+      color: ${({ theme }) => theme.COLORS.WHITE};
+    }
+  }
+`;
+
+export const Text = styled.div`
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 2; /* Número de linhas a serem exibidas antes do truncamento */
+  -webkit-box-orient: vertical;
+  
+  text-align: justify;
+  color: ${({ theme }) => theme.COLORS.GRAY_300};
+  margin-bottom: 28px;
 `;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
-import { Tag } from './components/Tag';
+import { Card } from './components/Card';
 import theme from './styles/theme.js';
 import GlobalStyles from './styles/global.js';
 import { FiUser, FiMail, FiLock } from 'react-icons/fi';
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Tag title="cachorro"/>
+      <Card />
     </ThemeProvider>
   </React.StrictMode>,
 );
